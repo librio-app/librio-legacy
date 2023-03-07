5896e73debf6ba580613c4e39b099087d47ed7fd
Joan Grinwis <joan@jgidee.nl>
2023-03-07 21:30:05 +0100

    update

--- resources/views/emails/memberportalcreated.blade.php
+++ resources/views/emails/memberportalcreated.blade.php
@@ -1,16 +1,15 @@
-@component('mail::message')
+<x-mail::message>
     # Beste {{$member->getName()}},
 
     Uw account voor {{config('app.name')}} is geactiveerd. Om toegang te krijgen dient u alleen nog een wachtwoord aan te maken.
 
-    @component('mail::button', ['url' => $activationUrl])
+    <x-mail::button :url="$activationUrl">
         Wachtwoord instellen
-    @endcomponent
+    </x-mail::button>
 
-    Werkt de knop niet? Kopieer dan de volgende link naar uw browser: {{$activationUrl}}
-
-
-    Met vriendelijke groet,
+    Met vriendelijke groet,<br>
 
     {{config('app.name')}}
-@endcomponent
+</x-mail::message>
+
+
