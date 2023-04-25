96db0fd4d89b6f63e3850891abfa0a22c7e705ec
Joan Grinwis <joan@jgidee.nl>
2023-04-25 20:16:43 +0200

    Added patern for repos and services

---
+++ app/Repositories/MemberArea/MemberRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Repositories\MemberArea;
+
+use App\Interfaces\MemberArea\Repositories\MemberRepositoryInterface;
+use App\Models\Administration\Member;
+use App\Repositories\BaseRepository;
+use Illuminate\Database\Eloquent\Model;
+
+class MemberRepository extends BaseRepository implements MemberRepositoryInterface
+{
+    public function __construct(Member $model)
+    {
+        parent::__construct($model);
+    }
+
+    public function find(int $id): ?Member
+    {
+        return parent::find($id);
+    }
+
+    public function findByConformationCode(string $confirmationCode): ?Member
+    {
+        $member = $this->model->where('confirmation_key', '=', $confirmationCode)->get();
+
+        if ($member)
+        {
+            return $member;
+        }
+
+        return null;
+    }
+}
