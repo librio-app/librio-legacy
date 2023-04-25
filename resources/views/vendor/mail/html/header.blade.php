8dfbbb755f11d32ca6824f41f8ed2e962449c43e
Michiel Kalle <m.kalle@xsarus.nl>
2023-04-25 21:08:21 +0200

    Use logo in mail

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-    <img src="{{ asset('images/librio-small.png') }}" class="logo" alt="Librio">
-</a>
-</td>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            <img src="{{ asset('images/librio-small.png') }}" class="logo" alt="Librio">
+        </a>
+    </td>
 </tr>
