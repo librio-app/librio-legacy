2a7b0139cbe007877e99d0069a7d20e4b7313809
Michiel Kalle <m.kalle@xsarus.nl>
2023-04-25 21:01:00 +0200

    Extra check

--- app/Repositories/MemberArea/MemberRepository.php
+++ app/Repositories/MemberArea/MemberRepository.php
@@ -22,6 +22,7 @@ class MemberRepository extends BaseRepository implements MemberRepositoryInterfa
     public function findByConfirmationCode(string $confirmationCode): ?Member
     {
         return $this->model->firstWhere([
+            ['enabled', '=', 1],
             ['confirmation_key', '=', $confirmationCode],
             ['confirmation_key_send_at', '>', Carbon::now()->addWeeks(1)]
         ]);
